10d39b59c89717b4e3ff33440387860170f02669
gsoldano <gsoldano27@aol.com>
2022-05-02 09:29:15 +0200

    Update

--- mdb_external/src/main/java/mdb_external/ConsumerMDB.java
+++ mdb_external/src/main/java/mdb_external/ConsumerMDB.java
@@ -11,9 +11,8 @@ import org.jboss.ejb3.annotation.ResourceAdapter;
 @MessageDriven(name = "ConsumerMDB", activationConfig = {
 		@ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "testQueueRemoteArtemis"),
 		@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
-        @ActivationConfigProperty(propertyName = "useJNDI", propertyValue = "false"),
-        @ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge")
-})
+		@ActivationConfigProperty(propertyName = "useJNDI", propertyValue = "false"),
+		@ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge") })
 @ResourceAdapter("remote-artemis")
 public class ConsumerMDB implements MessageListener {
 
--- mdb_external/src/main/java/mdb_external/ProducerServlet.java
+++ mdb_external/src/main/java/mdb_external/ProducerServlet.java
@@ -18,57 +18,57 @@ import javax.servlet.http.HttpServletResponse;
 public class ProducerServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
-	
-	@Inject
-    @JMSConnectionFactory("java:/jms/remoteCF")
-    private JMSContext context;
 
-    @Resource(lookup = "java:/queue/testQueueRemoteArtemis")
-    private Queue queue;
-	
+	@Inject
+	@JMSConnectionFactory("java:/jms/remoteCF")
+	private JMSContext context;
+
+	@Resource(lookup = "java:/queue/testQueueRemoteArtemis")
+	private Queue queue;
+
 	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		res.setContentType("text/html");
 
-        try (PrintWriter out = res.getWriter()) {
-        	out.println("<!DOCTYPE html>");
-        	out.println("<html>");
-        	
-        	out.println("<head>");
-        	out.println("<link rel=\"stylesheet\" href=\"resources/styles.css\">");
-        	out.println("</head>");
-        	
-        	out.println("<body>");
-        	out.println("<div class=\"main\">");
-        	
-        	out.println("<div class=\"logos\">");
-        	out.println("<img src=\"resources/wildfly.svg\">");
-        	out.println("<img src=\"resources/activemq.png\">");
-        	out.println("</div>");
-        	
-        	out.println("<form method=\"get\">");
-        	out.println("<h2>Send a message to remote ActiveMQ Artemis queue:</h2>");
-        	out.println("<input type=\"text\" name=\"message\" autocomplete=\"off\">");
-        	out.println("<p></p>");
-        	out.println("<div class=\"buttons\">");
-        	out.println("<input type=\"submit\" value=\"Send\"/>");
-        	out.println("<input type=\"reset\" value=\"Reset\"/>");
-        	out.println("</div>");
-        	out.println("</form>");
-        	
-        	String message = req.getParameter("message");
-        	
-        	if (message != null && message.length() > 0) {
-        		context.createProducer().send(queue, message);
-        		
-        		out.println("<h3>Message was sent. Check server console!</h3>");
-        	}
-        	
-        	out.println("</div>");
-        	out.println("</body>");
-        	
-        	out.println("</html>");
-        }
+		try (PrintWriter out = res.getWriter()) {
+			out.println("<!DOCTYPE html>");
+			out.println("<html>");
+
+			out.println("<head>");
+			out.println("<link rel=\"stylesheet\" href=\"resources/styles.css\">");
+			out.println("</head>");
+
+			out.println("<body>");
+			out.println("<div class=\"main\">");
+
+			out.println("<div class=\"logos\">");
+			out.println("<img src=\"resources/wildfly.svg\">");
+			out.println("<img src=\"resources/activemq.png\">");
+			out.println("</div>");
+
+			out.println("<form method=\"get\">");
+			out.println("<h2>Send a message to remote ActiveMQ Artemis queue:</h2>");
+			out.println("<input type=\"text\" name=\"message\" autocomplete=\"off\">");
+			out.println("<p></p>");
+			out.println("<div class=\"buttons\">");
+			out.println("<input type=\"submit\" value=\"Send\"/>");
+			out.println("<input type=\"reset\" value=\"Reset\"/>");
+			out.println("</div>");
+			out.println("</form>");
+
+			String message = req.getParameter("message");
+
+			if (message != null && message.length() > 0) {
+				context.createProducer().send(queue, message);
+
+				out.println("<h3>Message was sent. Check server console!</h3>");
+			}
+
+			out.println("</div>");
+			out.println("</body>");
+
+			out.println("</html>");
+		}
 	}
 
 }
